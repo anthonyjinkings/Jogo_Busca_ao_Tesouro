dbe120c6397fa2d0120ec8c6285f9ab74df858fa
Anthony Jinkings <anthonyjinkings2@gmail.com>
2024-08-03 21:34:34 -0300

    Add files via upload

--- game2.cpp
+++ game2.cpp
@@ -3,25 +3,50 @@
 #include <ctime>
 #include <iomanip>
 #include <stdlib.h>
+#include <windows.h>
 #include <conio.h>
 #include <locale>
 #include <codecvt>
-#define ANSI_COLOR_CYN      "\e[0;36m" //cores em ANSI utilizadas 
-#define ANSI_COLOR_RED     "\e[0;31m"
+#define ANSI_COLOR_CYN      "\e[0;36m" // Cor ciano
+#define ANSI_COLOR_RED      "\e[0;31m" // Cor vermelha
+#define ANSI_COLOR_YEL      "\e[0;33m" // Cor amarela
+#define ANSI_COLOR_RESET    "\e[0m"    // Resetar cor
 
 
 using namespace std;
 
+void imprimirmatriz(const string matriz[6][6]) {
+    cout << ANSI_COLOR_CYN << "+-----+-----+-----+-----+-----+-----+" << ANSI_COLOR_RESET << endl;
+    for (int l=0; l<6; l++) {
+        cout << ANSI_COLOR_CYN << "| " << ANSI_COLOR_RESET;
+        for (int c=0; c<6; c++) {
+            cout << setw(3) << matriz[l][c] << ANSI_COLOR_CYN << " | " << ANSI_COLOR_RESET;
+        }
+        cout << endl << ANSI_COLOR_CYN << "+-----+-----+-----+-----+-----+-----+" << ANSI_COLOR_RESET << endl;
+    }
+}
+
+void exibirranking(const int pontuacao[], const string nomes[], int num_jog) {
+    cout << ANSI_COLOR_CYN << "Ranking atual:" << ANSI_COLOR_RESET << endl;
+    for (int i=0; i< num_jog; i++) {
+        cout << ANSI_COLOR_RED << "Jogador " << i + 1 << " (" << nomes[i] << "): " << pontuacao[i] << " quilates" << ANSI_COLOR_RESET << endl;
+    }
+}
+
 int main() {
     system("chcp 65001 > nul");
-    int num_jog, perso1, perso2, perso3, perso4, nume_jog=1, escolha, quilates[6][6], totaljogadas = 36;
-    string perso5[5];
-    string nome_jog1, nome_jog2, nome_jog3, nome_jog4, emoji[]={"🐻", "😽", "🧙‍♂", "🦸‍♂"};
-    string emoji_matriz[]={"💎"};
     int escolhas[5];
     int i;
     float armadilhas = 0.10;
     float dicas = 0.05;
+    int num_jog, perso1, perso2, perso3, perso4, nume_jog=1, escolha, quilates[6][6], totaljogadas = 36;
+    string perso5[5];
+    string nome_jog1, nome_jog2, nome_jog3, nome_jog4, emoji[]={"🐻", "😽", "🧙‍♂", "🦸‍♂"};
+    string emoji_matriz[]={"💎"};
+    string matriz[6][6];
+    string emoji_trevo[]={"🍀"};
+    string emoji_armadilha[]={"🧨"};
+    string emoji_dica[]{"💰"};
     cout << "Quantos jogadores teremos nessa partida?" << endl;
     cin >> num_jog;
     while (num_jog < 2 || num_jog > 4) {
@@ -49,33 +74,74 @@ int main() {
             cout << ANSI_COLOR_RED << "Personagem do jogador " << i+1 << ":" << emoji[escolhas[i]-1] << endl;
         }
          //--------------------------------Parte dois adicional----------------------------------------//
-        string matriz[6][6];
-        int l, c;
         srand(time(NULL));
-        for (l=0; l<6; l++) {
-            for (c=0; c<6; c++) {
+        int totalpontos = 6*6;
+        int totalarmadilhas = armadilhas * totalpontos;
+        int totalpistas = dicas * totalpontos;
+        int l, c;
+
+        for (l = 0; l<6; l++) {
+            for (c = 0; c<6; c++) {
                 matriz[l][c] = emoji_matriz[0];
-                quilates[l][c] = rand()%10 + 1;
             }
         }
-        //--Armadilhas--//
-    int total_armadilhas = armadilhas * 6 * 6;
-        for (int i = 0; i < total_armadilhas; i++) {
-            int l = rand() % 6;
-            int c = rand() % 6;
-            matriz[l][c] = (i % 2 == 0) ? "O" : "-"; // Alterna entre as duas armadilhas
-    }
-    // Distribuição de pistas
-    int total_pistas = dicas * 6 * 6;
-    for (int i = 0; i < total_pistas; i++) {
-        int l = rand() % 6;
-        int c = rand() % 6;
-        matriz[l][c] = "+"; // Pista
-    }
-    for (l=0; l<6; l++) {
-            for (c=0; c<6; c++) {
-                cout << setw(5) << matriz[l][c];
+
+        int num_armadilhas =0, num_pistas = 0;
+
+        while (num_armadilhas < totalarmadilhas) {
+            l = rand()%6;
+            c = rand()%6;
+            if (matriz[l][c] == emoji_matriz[0]) {
+                matriz[l][c] = emoji_trevo[0];
+                num_armadilhas++;
             }
-            cout << endl;
+        }
+
+        while (num_pistas < totalpistas) {
+            l = rand()%6;
+            c = rand()%6;
+            if (matriz[l][c] == emoji_matriz[0]) {
+                matriz[l][c] = emoji_trevo[0];
+                num_pistas++;
+            }
+        }
+
+        int pontuacao[4] = {0};
+
+        for (int jogada =0; jogada < 36; jogada++) {
+            imprimirmatriz(matriz);
+            exibirranking(pontuacao, perso5, num_jog);
+            int jogadoratual = jogada % num_jog;
+            cout << ANSI_COLOR_CYN << "Jogador " << jogadoratual + 1 << " (" << perso5[jogadoratual] << ") Escolha uma posição: " << ANSI_COLOR_RESET;
+            int linha, coluna;
+            cin >> linha >> coluna;
+
+            if (linha >= 0 && linha < 6 && coluna >= 0 && coluna < 6) {
+                string conteudo = matriz[linha][coluna];
+                if (conteudo == emoji_trevo[0]) {
+                    if (rand() % 2 == 0) {
+                        conteudo = emoji_armadilha[0];
+                        pontuacao[jogadoratual] -= 10;
+                        cout << ANSI_COLOR_RED << "O jogador " << jogadoratual + 1 << "caiu em uma bomba e perdeu 10 quilates!" << ANSI_COLOR_RESET << endl;
+
+                    } else {
+                        conteudo = emoji_dica[0];
+                        pontuacao[jogadoratual] += 10;
+                        cout << ANSI_COLOR_YEL << "O jogador " << jogadoratual + 1 << "encontrou um saco cheio de ouro! Mais 10 quilates pra conta." << ANSI_COLOR_RESET << endl;
+                    }
+                } else if (conteudo == emoji_matriz[0]) {
+                    int diamantes = rand() % 10 + 1;
+                    pontuacao[jogadoratual] += diamantes;
+                    cout << ANSI_COLOR_YEL << "O jogador " << jogadoratual + 1 << "encontrou " << diamantes << " quilates de diamantes!" << ANSI_COLOR_RESET << endl;
+                }
+                matriz[linha][coluna] = emoji[escolhas[jogadoratual] - 1];
+            } else {
+                cout << ANSI_COLOR_RED << "Posição inválida! Tente novamente." << ANSI_COLOR_RESET << endl;
+                jogada--;
+            }
+        }
+        cout << ANSI_COLOR_YEL << "O ranking final é: " << ANSI_COLOR_RESET << endl;
+        for (int i=0; i< num_jog; i++) {
+        cout << ANSI_COLOR_YEL << "Jogador " << i + 1 << " (" << perso5[i] << "): " << pontuacao[i] << " quilates" << ANSI_COLOR_RESET << endl;
         }
     }
